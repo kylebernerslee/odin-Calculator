6cd9747d7d04b39ed2b275ed3ac099636de7eed6
kylebernerslee <159580020+kylebernerslee@users.noreply.github.com>
2024-03-23 10:48:34 +0100

    Bare Bones Setup for Calculations.

--- code.js
+++ code.js
@@ -4,22 +4,83 @@ const screenSolution = document.querySelector("div.screenSolution");
 const resultButton = document.querySelector("div.result");
 const resetButton = document.querySelector("div.reset");
 let calculation;
+let number1 = "";
+let number2 = "";
+let operator = "";
 
 numbers.forEach((button) => {
-    button.addEventListener("click", getNumber, true);
+    button.addEventListener("click", getValue, true);
 });
 
 resultButton.addEventListener("click", getResult, true);
 
-function getNumber() {
-    if(this.innerText != "=")
-        screenProgress.textContent += this.innerText;
+function getValue() {
+
+
+    screenProgress.innerText += this.innerText;
+
+    if(this.innerText != "+" || "-" || ":" || "/")
+    {
+        if(operator != "")
+        {
+            number2 += this.innerText;
+        }
+        else
+        {
+            number1 += this.innerText;
+        }
+    }
+    else
+    {
+        if(operator != "")
+        {
+
+        }
+        else
+        {
+            operator += this.innerText;
+        }
+    }
+
+
+    console.log(number1, number2, operator);
+
 }
 
 function getResult() {
-    calculation = screenProgress.textContent.split("+");
-    screenProgress.textContent = "";
-    let result = parseInt(calculation[0]) + parseInt(calculation[1]);
-    screenSolution.textContent = result;
+    
+    switch (operator) {
+        case "+":
+            break;
+        case "-":
+            break;
+        case "*":
+            break;
+        case "/":
+            break;
+    }
+    
+    number1 = "";
+    number2 = "";
+    operator = "";
+}
 
+function add (num1, num2)
+{
+    return parseInt(num1) + parseInt(num2);
+}
+
+function subtract (num1, num2)
+{
+    return parseInt(num1) - parseInt(num2);
+}
+
+function multiply(num1, num2)
+{
+    return parseInt(num1) * parseInt(num2);
+}
+
+function subtract(num1, num2)
+{
+    return parseInt(num1) / parseInt(num2);
 }
